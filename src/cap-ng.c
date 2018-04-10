stevegrubb/libcap-ng

commit 7759e6f8469eb33aef1a1d5eba5d300c3a8fcb63

Use pthread_atfork to optionally reset the pid and related info on fork

src/cap-ng.c
@@ -1,5 +1,5 @@
 /* libcap-ng.c --
- * Copyright 2009-10, 2013 Red Hat Inc., Durham, North Carolina.
+ * Copyright 2009-10, 2013, 2017 Red Hat Inc., Durham, North Carolina.
  * All Rights Reserved.
  *
  * This library is free software; you can redistribute it and/or
@@ -34,6 +34,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <byteswap.h>
+#include <pthread.h>	// For pthread_atfork
 #ifdef HAVE_SYSCALL_H
 #include <sys/syscall.h>
 #endif
@@ -152,6 +153,29 @@ static __thread struct cap_ng m =	{ 1,
 					CAPNG_NEW,
 					{0, 0} };
 
+
+/*
+ * The pthread_atfork function is being made weak so that we can use it
+ * if the program is linked with pthreads and not requiring it for
+ * everything that uses libcap-ng.
+ */
+extern int __attribute__((weak)) pthread_atfork(void (*prepare)(void),
+	void (*parent)(void), void (*child)(void));
+
+/*
+ * Reset the state so that init gets called to erase everything
+ */
+static void deinit(void)
+{
+	m.state = CAPNG_NEW;
+}
+
+static void init_lib(void) __attribute__ ((constructor));
+static void init_lib(void)
+{
+	if (pthread_atfork)
+		pthread_atfork(NULL, NULL, deinit);
+}
 
 static void init(void)
 {

docs/capng_setpid.3
@@ -10,6 +10,16 @@ void capng_setpid(int pid);
 
 capng_pid sets the working pid for capabilities operations. This is useful if you want to get the capabilities of a different process.
 
+.SH NOTES
+
+If your process calls 
+.B fork
+, then the child process will still have the pid of the parent process stored in libcap-ng's internal data. It is disallowed to do any kind of setcap operations because you would be crossing process boundaries. To correct this, if your program links against pthreads, then libcap-ng will use the 
+.B pthread_atfork
+function (as a weak symbol) to reset the pid information to the new process automatically. You are not required to link against pthreads. You can call 
+.B capng_setpid
+and adjust the stored pid manually.
+
 .SH "RETURN VALUE"
 
 None.

commit 1d28cf070933568db8006ce45411723c5369e145

Fix compilation without pthread.h
Commit 7759e6f8469eb33aef1a1d5eba5d300c3a8fcb63 broke compilation on
systems without pthread.h. So add a call to AC_CHECK_HEADERS in
configure.ac and put include <pthread.h> under HAVE_PTHREAD_H define

Fixes:
 - http://autobuild.buildroot.net/results/6132f33fb282fda3c39deb292784b9006c9e7872

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>

configure.ac
@@ -56,6 +56,7 @@ AC_CHECK_HEADERS(sys/xattr.h, [], [
 	AC_CHECK_HEADERS(attr/xattr.h, [], [AC_MSG_WARN(attr/xattr.h not found, disabling file system capabilities.)])
 	])
 AC_CHECK_HEADERS(linux/securebits.h, [], [])
+AC_CHECK_HEADERS(pthread.h, [], [AC_MSG_WARN(pthread.h not found, disabling pthread_atfork.)])
 
 AC_C_CONST
 AC_C_INLINE

src/cap-ng.c
@@ -34,7 +34,9 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <byteswap.h>
+#ifdef HAVE_PTHREAD_H
 #include <pthread.h>	// For pthread_atfork
+#endif
 #ifdef HAVE_SYSCALL_H
 #include <sys/syscall.h>
 #endif