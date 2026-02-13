2683fbf1c36ef4cbf6965066bf78e6635180bfa5
Steve Grubb <ausearch.1@gmail.com>
2026-02-12 19:59:48 -0500

    Add license info

--- utils/netcap-advanced.c
+++ utils/netcap-advanced.c
@@ -1,3 +1,27 @@
+/*
+ * netcap-advanced.c - Advanced capability analysis
+ * Copyright (c) 2026 Steve Grubb
+ * All Rights Reserved.
+ *
+ * This software may be freely redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor
+ * Boston, MA 02110-1335, USA.
+ *
+ * Authors:
+ *   Steve Grubb <sgrubb@redhat.com>
+ */
+
 #include "config.h"
 #include <arpa/inet.h>
 #include <dirent.h>
